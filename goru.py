63c8df1d514f0301f221b87874088abed96e4d39
xy1999729 <32480633+xy1999729@users.noreply.github.com>
2021-11-14 23:27:54 +0800

    Update goru.py

--- goru.py
+++ goru.py
@@ -104,10 +104,10 @@ def fft_param(num_units):
     phase_init = tf.random_uniform_initializer(-3.14, 3.14)
     capacity = int(math.log(num_units, 2))
 
-    gorutheta = tf.get_variable("gorutheta", [capacity, num_units // 2],
+    theta = tf.get_variable("theta", [capacity, num_units // 2],
                             initializer=phase_init)
-    cos_theta = tf.cos(gorutheta)
-    sin_theta = tf.sin(gorutheta)
+    cos_theta = tf.cos(theta)
+    sin_theta = tf.sin(theta)
 
     cos_list = tf.concat([cos_theta, cos_theta], axis=1)
     sin_list = tf.concat([sin_theta, -sin_theta], axis=1)
